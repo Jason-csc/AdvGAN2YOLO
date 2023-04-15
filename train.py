334179e7d755b94d642e4668c2c9374fe8115a64
longjie3 <longjie3@illinois.edu>
2023-04-15 18:50:28 -0500

    add label wise dataset

--- train.py
+++ train.py
@@ -1,43 +1,58 @@
+import argparse
 import os
 
 import torch
-import torchvision.datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
+from dataset import MINISTLableWiseInMemoDataset
 from model import AdvModel
 from resnet18 import Resnet18
 
-BATCH_SIZE = 128
-EPOCH = 60
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--lbl", type=int, default=0, help="label of images used for training")
+parser.add_argument("--lamda", type=float, default=0.1, help="default weight of adv loss")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--n_cpu", type=int, default=1, help="number of cpu threads to use during batch generation")
+parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+parser.add_argument("--img_size", type=int, default=28, help="size of each image dimension")
+parser.add_argument("--channels", type=int, default=1, help="number of image channels")
+parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
+opt = parser.parse_args()
+
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+IMAGE_SHAPE = (opt.channels, opt.img_size, opt.img_size)
 
 MODEL_PATH = "./model/"
 if not os.path.exists(MODEL_PATH):
     os.makedirs(MODEL_PATH)
-
 
 GRAYSCALE = True
 NUM_CLASSES = 10
 
 
 TARGET_MODEL_PATH = "./target_model/resnet18_minst_best.pth"
-print(f"Loadign target model from {TARGET_MODEL_PATH}")
+print(f"Loading target model from {TARGET_MODEL_PATH}")
 target_model =  Resnet18(NUM_CLASSES,GRAYSCALE)
 target_model.load_state_dict(torch.load(TARGET_MODEL_PATH))
 target_model.to(DEVICE)
 target_model.eval()
 
 
-mnist_dataset = torchvision.datasets.MNIST('./data', train=True, transform=transforms.ToTensor(), download=True)
-train_dataloader = DataLoader(mnist_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=1)
+mnist_dataset = MINISTLableWiseInMemoDataset('./data', lbl = opt.lbl, train=True, transform=transforms.ToTensor())
+train_dataloader = DataLoader(mnist_dataset, batch_size=opt.batch_size, shuffle=True, num_workers=opt.n_cpu)
 
 
-advmodel = AdvModel(device=DEVICE)
+advmodel = AdvModel(latent_dim=opt.latent_dim, lbl=opt.lbl, img_shape=IMAGE_SHAPE, \
+                    lamda=opt.lamda, lr=opt.lr, momentum=(opt.b1, opt.b2), device=DEVICE)
 
 
-for epoch in range(EPOCH):
+for epoch in range(opt.n_epochs):
     batch_compound_loss = 0
     batch_d_loss = 0
     for i, (imgs, _) in enumerate(train_dataloader):
@@ -55,52 +70,3 @@ for epoch in range(EPOCH):
     if epoch%20 == 0:
         filename = MODEL_PATH  + f'advGenerator_epoch_{epoch}.pth'
         torch.save(advmodel.generator.state_dict(), filename)
-        
-
-         
-
-
-
-
-
-
-
-def train(self, train_dataloader, epochs):
-        for epoch in range(1, epochs+1):
-
-            if epoch == 50:
-                self.optimizer_G = torch.optim.Adam(self.netG.parameters(),
-                                                    lr=0.0001)
-                self.optimizer_D = torch.optim.Adam(self.netDisc.parameters(),
-                                                    lr=0.0001)
-            if epoch == 80:
-                self.optimizer_G = torch.optim.Adam(self.netG.parameters(),
-                                                    lr=0.00001)
-                self.optimizer_D = torch.optim.Adam(self.netDisc.parameters(),
-                                                    lr=0.00001)
-            loss_D_sum = 0
-            loss_G_fake_sum = 0
-            loss_perturb_sum = 0
-            loss_adv_sum = 0
-            for i, data in enumerate(train_dataloader, start=0):
-                images, labels = data
-                images, labels = images.to(self.device), labels.to(self.device)
-
-                loss_D_batch, loss_G_fake_batch, loss_perturb_batch, loss_adv_batch = \
-                    self.train_batch(images, labels)
-                loss_D_sum += loss_D_batch
-                loss_G_fake_sum += loss_G_fake_batch
-                loss_perturb_sum += loss_perturb_batch
-                loss_adv_sum += loss_adv_batch
-
-            # print statistics
-            num_batch = len(train_dataloader)
-            print("epoch %d:\nloss_D: %.3f, loss_G_fake: %.3f,\
-             \nloss_perturb: %.3f, loss_adv: %.3f, \n" %
-                  (epoch, loss_D_sum/num_batch, loss_G_fake_sum/num_batch,
-                   loss_perturb_sum/num_batch, loss_adv_sum/num_batch))
-
-            # save generator
-            if epoch%20==0:
-                netG_file_name = MODEL_PATH + 'netG_epoch_' + str(epoch) + '.pth'
-                torch.save(self.netG.state_dict(), netG_file_name)
